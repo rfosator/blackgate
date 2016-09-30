fa3d6a136f7f7f513fb421dc15a9369e9c6262ac
rfosator <ramonft@gmail.com>
2016-09-29 19:25:01 -0500

    Se agrega repositorio genérico

---
+++ Blackgate/Blackgate.DataModel/Context.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Blackgate.DataModel
+{
+    public class EfContext<TEntity> : DbContext where TEntity : class
+    {
+        public EfContext(string nameOrStringConnection) : base(nameOrStringConnection)
+        {
+        }
+
+        public IDbSet<TEntity> Entity { get; set; }
+    }
+}
---
+++ Blackgate/Blackgate.DataModel/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Blackgate.DataModel
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        private EfContext<TEntity> context;
+        public Repository(string nameOrConnectionString)
+        {
+            context = new EfContext<TEntity>(nameOrConnectionString);
+        }
+
+        public TEntity GetById<TKey>(TKey id)
+        {
+            return context.Entity.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            context.Entity.Add(entity);            
+        }
+
+        public void Update(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
